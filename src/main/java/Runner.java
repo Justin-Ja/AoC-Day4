1b18d1426c2cfe6d1e30fcdb9924ec6574651a42
Justin J <95664856+Justin-Ja@users.noreply.github.com>
2022-12-04 01:25:43 -0500

    Add files via upload

---
+++ src/main/java/Runner.java
@@ -0,0 +1,53 @@
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.stream.Stream;
+
+
+public class Runner {
+
+    public static void main(String args[]){
+        //Runner main = new Runner();
+        Load load = new Load("input.txt", "assets"); 
+        String input = load.readTheFile();
+        Scanner scan = new Scanner(input);
+        int total = 0;
+        String test = "", line = "";
+        int[] nums = new int[5];
+
+        //Part 1
+        while(scan.hasNextLine()){ 
+            line = scan.nextLine(); 
+            test = line.replaceAll("-", ",");
+            for(int i = 0; i < 4; i++){
+                nums[i] = Integer.parseInt(test.split(",")[i]);
+            }
+
+            if(nums[0] <= nums[2] && nums[1] >= nums[3]){
+                total++;
+            } else if(nums[2] <= nums[0] && nums[3] >= nums[1]){
+                total++;
+            }
+        }
+
+        System.out.println(total);
+
+        //Part 2
+        while(scan.hasNextLine()){ 
+            line = scan.nextLine(); 
+            test = line.replaceAll("-", ",");
+            for(int i = 0; i < 4; i++){
+                nums[i] = Integer.parseInt(test.split(",")[i]);
+            }
+
+            if(nums[0] <= nums[2] && nums[1] >= nums[3] || nums[1] >= nums[2] && nums[3] >= nums[0]){
+                total++;
+            } else if(nums[2] <= nums[0] && nums[3] >= nums[1]){
+                total++;
+            }
+        }
+
+        System.out.println(total);
+
+    }
+}
+
